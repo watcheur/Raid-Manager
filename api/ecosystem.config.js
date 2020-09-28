948654e8f876c1da4fa049f32ffbfeb242f4b712
Watcheur <moriceau.mathieu@gmail.com>
2020-09-28 17:41:41 +0200

    Update pm2 ecosystem file

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -2,6 +2,12 @@ module.exports = {
     apps : [{
         name: "Raid-Manager API",
         script: "./server.js",
-        node_args: '-r dotenv/config'
+        node_args: '-r dotenv/config',
+        env: {
+            NODE_ENV: "development"
+        },
+        env_production: {
+            NODE_ENV: "production"
+        }
     }]
 }
